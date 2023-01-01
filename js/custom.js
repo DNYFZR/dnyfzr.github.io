1d939dfe9349ea9668d462e94e669c2105d0011e
Daniel <76977155+DNYFZR@users.noreply.github.com>
2023-01-01 16:37:16 +0000

    JS update

--- js/custom.js
+++ js/custom.js
@@ -3,9 +3,9 @@
   "use strict";
 
     // COLOR MODE
-    $('.color-mode').click(function(){
-        $('.color-mode-icon').toggleClass('active')
-        $('body').toggleClass('dark-mode')
+    $('.dark-mode').click(function(){
+        $('.color-mode-icon.active').toggleClass('active')
+        $('body').toggleClass('color-mode')
     })
 
     // HEADER
